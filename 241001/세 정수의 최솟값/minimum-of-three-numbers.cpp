d0b4f180d509a1b7a4fbd2bdb841c5a77e3ed86c
ramenghost3558 <138766903+ramenghost3558@users.noreply.github.com>
2024-10-01 00:49:06 +0900

    Title: 세 정수의 최솟값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 241001/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a, b, c, an;
     cin >> a >> b >> c;
-    an = a < b && a < c ? a : (b < a && b < c ? b : c);
+    an = a <= b && a <= c ? a : (b <= a && b <= c ? b : c);
     cout << an;
     return 0;
 }
