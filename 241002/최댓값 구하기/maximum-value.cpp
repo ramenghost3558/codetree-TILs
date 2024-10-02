edff284634a2ea33da9b1b494d5b0e0e70a93681
ramenghost3558 <138766903+ramenghost3558@users.noreply.github.com>
2024-10-02 23:55:34 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/최댓값 구하기/maximum-value.cpp
+++ 241002/최댓값 구하기/maximum-value.cpp
@@ -5,7 +5,7 @@ int main() {
     int a, b, c, m;
     cin >> a >> b >> c;
 
-    m = a > b && a > c ? a : (b > a && b > c ? b : c);
+    m = a >= b && a >= c ? a : (b >= a && b >= c ? b : c);
     cout << m;
     return 0;
 }
